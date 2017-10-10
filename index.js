0ef1a35331343cde878e1953ecc9cb99d701e982
nathancoblentz <nathancoblentz@gmail.com>
2017-10-10 19:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+  push.kittens("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  unshift.kittens("Bob")
+  return kittens
+}
