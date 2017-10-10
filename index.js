af37a89e7c49915b53830000d2c573e85e8c8ecf
nathancoblentz <nathancoblentz@gmail.com>
2017-10-10 19:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function destructivelyRemoveFirstKitten () {
   kittens.shift()
   return kittens
 }
+
+function appendKitten(name) {
+  MOARKittehs = [...kittens, "Broom"]
+  return MOARKittehs
+}
+function prependKitten(name) {
+  MOARKittehs = ["Arnold", ...kittens]
+  return MOARKittehs
+}
+function removeLastKitten () {
+  lessKittehs = kittens.slice(0, kittens.length - 1)
+  return lessKittehs
+}
+function removeFirstKitten () {
+  lessKittehs = kittens.slice(1)
+  return lessKittehs
+}
