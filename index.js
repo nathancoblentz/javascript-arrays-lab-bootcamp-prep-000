742a2e79a39c9c792222a48526adb3acdf57d6b9
nathancoblentz <nathancoblentz@gmail.com>
2017-10-10 19:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function destructivelyPrependKitten(name) {
   kittens.unshift("Bob")
   return kittens
 }
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+  return kittens 
+}
+  
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+  return kittens
+}
+  
